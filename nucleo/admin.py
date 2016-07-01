97877036b09972c3c0c2ca1216fa5cd57a3937e8
samuels <samuelsas.samuel18@gmail.com>
2016-07-01 17:14:23 -0300

    continuando admins

--- nucleo/admin.py
+++ nucleo/admin.py
@@ -2,16 +2,18 @@ from django.contrib import admin
 from .models import Missao,Aniversario,Doacao,Media
 
 
-@admin.register(Missao,Aniversario,Doacao,Media)
-
+@admin.register(Missao)
 class MissaoAdmin(admin.ModelAdmin):
     pass
 
+@admin.register(Aniversario)
 class AniversarioAdmin(admin.ModelAdmin):
     pass
 
+@admin.register(Doacao)
 class DoacaoAdmin(admin.ModelAdmin):
     pass
 
+@admin.register(Media)
 class MediaAdmin(admin.ModelAdmin):
     pass
--- nucleo/models.py
+++ nucleo/models.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from __future__ import unicode_literals
 
 from django.db import models
@@ -5,13 +6,16 @@ from ordered_model.models import OrderedModel
 from financeiro.models import Pagamento
 
 class Missao(models.Model):
-    titulo = models.CharField(max_length=255)
-    descricao = models.TextField()
-    slug = models.SlugField(blank=True,max_length=255)
+    titulo = models.CharField('Titulo',max_length=255)
+    descricao = models.TextField('Descrição')
+    slug = models.SlugField('Slug',blank=True,max_length=255)
+
+    def __unicode__(self):
+        return self.titulo
 
 class Aniversario(models.Model):
     missao = models.ForeignKey(Missao)
-    ano = models.IntegerField()
+    ano = models.IntegerField('Ano')
 
 class Doacao(models.Model):
     aniversario = models.ForeignKey(Aniversario)
