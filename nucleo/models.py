03abc0bbed9083b6037f9f4b96fb656f23850371
samuels <samuelsas.samuel18@gmail.com>
2016-07-01 16:38:10 -0300

    add models

--- nucleo/models.py
+++ nucleo/models.py
@@ -1,5 +1,24 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from ordered_model.models import OrderedModel
 
-# Create your models here.
+class Missao(models.Model):
+    titulo = models.CharField(max_length=255)
+    descricao = models.TextField()
+    slug = models.SlugField()
+
+class Aniversario(models.Model):
+    missao = models.ForeignKey(Missao)
+    ano = models.DateField()
+
+class Doacao(models.Model):
+    aniversario = models.ForeignKey(Aniversario)
+
+class Media(OrderedModel):
+
+    class Meta(OrderedModel.Meta):
+        pass
+
+    missao = models.ForeignKey(Missao)
+    arquivo = models.FileField()
