a67598549b0a2ad785a3f833343d59e10be32768
samuels <samuelsas.samuel18@gmail.com>
2016-07-04 15:30:14 -0300

    add view missao

--- nucleo/urls.py
+++ nucleo/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^criar/$', views.criarMissao, name='criarMissao'),
+    url(r'^criar/$', views.criar_missao, name='criar_missao'),
+    url(r'^(?P<slug>[\w-]+)/$', views.missao, name='missao'),
 ]
--- nucleo/views.py
+++ nucleo/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 from .forms import MissaoForm
+from .models import Missao
 
-def criarMissao(request):
+@login_required
+def criar_missao(request):
     form = MissaoForm(request.POST or None)
     if form.is_valid():
         form.save()
-    return render(request,'nucleo/criarmissao.html', {
+    return render(request, 'nucleo/criarmissao.html', {
         'form':form
     })
+
+def missao(request,slug):
+    missao = Missao.objects.get(slug=slug)
+    return render(request, 'nucleo/missao.html',{'missao':missao})
