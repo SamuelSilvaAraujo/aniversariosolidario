e13c6e514b1ddbc2b682e2279a96766246b82f1b
Douglas Paz <eu@douglaspaz.com>
2016-07-04 12:15:58 -0300

    alterado menu de navegação

--- webapp/views.py
+++ webapp/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'webapp/index.html')
+
+def styleguide(request):
+    return render(request, 'webapp/styleguide.html')
