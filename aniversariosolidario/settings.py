b00028f7d1286990f05d58eec5fe33bfb63e6d81
samuels <samuelsas.samuel18@gmail.com>
2016-07-05 10:52:44 -0300

    update settings e urls

--- aniversariosolidario/settings.py
+++ aniversariosolidario/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 LOGIN_URL='/usuario/entrar/'
 LOGIN_REDIRECT_URL='/'
