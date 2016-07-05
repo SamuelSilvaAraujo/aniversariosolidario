72fbaea9fb1cacd7dadcf20fe496dcda6e3cc559
Douglas Paz <eu@douglaspaz.com>
2016-07-05 10:17:12 -0300

    add url styleguide e corrigindo bootstrap_base

--- aniversariosolidario/urls.py
+++ aniversariosolidario/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from webapp import views as webapp_views
 from usuarios import urls as usuarios_urls
 from nucleo import urls as nucleo_urls
+from webapp import views as webapp_views
 
 urlpatterns = [
     url(r'^$', webapp_views.index, name='index'),
     url(r'^usuario/', include(usuarios_urls, namespace='usuarios')),
-    url(r'^missao/',include(nucleo_urls,namespace='nucleo')),
+    url(r'^missao/', include(nucleo_urls, namespace='nucleo')),
+    url(r'^styleguide/$', webapp_views.styleguide, name='styleguide'),
     url(r'^admin/', admin.site.urls),
 ]
