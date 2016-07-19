6513dfc73a76fa501842517a383ef1b6bb253919
samuels <samuelsas.samuel18@gmail.com>
2016-07-19 13:28:17 -0300

    add integraçao com twitter

--- aniversariosolidario/settings.py
+++ aniversariosolidario/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.twitter',
     'usuarios',
     'nucleo',
     'financeiro',
