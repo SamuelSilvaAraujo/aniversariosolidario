ac24f31c9cb718cd461d410fc58bd5a7edfaa305
Douglas Paz <eu@douglaspaz.com>
2016-07-22 08:14:06 -0300

    corrigindo footer flutuante e corrigido tamanho do no avatar micro

--- webapp/static/bootstrap/dist/js/bootstrap.js
+++ webapp/static/bootstrap/dist/js/bootstrap.js
@@ -2409,3 +2409,24 @@ var data_name = 'panel-height-fixed';
   render();
   $(window).resize($.debounce(500, render));
 }(jQuery);
+
++function ($) {
+  'use strict';
+
+  $(window).scroll(function (e){
+    var
+      $footer = $('footer'),
+      $footer_floating = $('.footer-floating'),
+      footer_top = $footer.offset().top,
+      footer_height = $footer.innerHeight(),
+      distance_footer = $(this).scrollTop() + $(this).height();
+
+    if(distance_footer > footer_top){
+      $footer_floating.css('position', 'absolute');
+      $footer_floating.css('bottom', footer_height + 'px');
+    } else {
+      $footer_floating.css('position', 'fixed');
+      $footer_floating.css('bottom', '0');
+    }
+  });
+}(jQuery);
