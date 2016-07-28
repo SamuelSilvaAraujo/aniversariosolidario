4d1027289bc60260b37c36973a881fef16c45962
Douglas Paz <eu@douglaspaz.com>
2016-07-28 08:41:48 -0300

    add enviar email doacao completa

--- financeiro/models.py
+++ financeiro/models.py
@@ -3,8 +3,15 @@ from __future__ import unicode_literals
 
 import logging
 
+from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.urlresolvers import reverse
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils import timezone
+from emails.models import ContaDeEmail
+from emails.models import Email
 from pagseguro.models import Checkout, TRANSACTION_STATUS_CHOICES
 from pagseguro.signals import notificacao_recebida, update_transaction
 from usuarios.models import Usuario
@@ -21,7 +28,10 @@ class Pagamento(models.Model):
     cartao = models.ForeignKey(Checkout, related_name='pagamentos_por_cartao', null=True, blank=True)
 
     def __unicode__(self):
-        return '{}'.format(self.status_verbose)
+        return 'Pagamento {} - {}'.format(
+            ', '.join(map(lambda x: unicode(x), self.doacoes.all())),
+            self.status_verbose
+        )
 
     @property
     def status_verbose(self):
@@ -30,6 +40,29 @@ class Pagamento(models.Model):
     @property
     def status_valido(self):
         return self.status in ['pago', 'disponivel']
+
+    def enviar_email_doacao_completa(self):
+        for doacao in self.doacoes.all():
+            email = Email.objects.create(
+                de=ContaDeEmail.get_naoresponda(),
+                para_email=doacao.aniversario.usuario.email,
+                assunto='Doação de R${} recebida'.format(self.valor)
+            )
+            email.carregar_corpo(
+                'financeiro/emails/doacao_completa.txt',
+                'financeiro/emails/doacao_completa.html',
+                nome=doacao.usuario_nome,
+                valor=self.valor,
+                beneficiado=doacao.aniversario.missao.beneficiado,
+                url_painel='{}{}'.format(settings.FULL_URL, reverse('usuarios:index'))
+            )
+            email.enviar_as = timezone.now()
+            email.save(update_fields=['enviar_as'])
+
+@receiver(post_save, sender=Pagamento)
+def post_save_Pagamento(instance, update_fields, **kwargs):
+    if update_fields and 'status' in update_fields and instance.status == 'pago':
+        instance.enviar_email_doacao_completa()
 
 class Transacao(models.Model):
     valor = models.IntegerField()
