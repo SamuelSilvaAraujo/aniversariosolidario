6547f460b9a2bb74998c7417488927ca73a80be6
Douglas Paz <eu@douglaspaz.com>
2016-10-03 11:22:38 -0300

    corrigindo busca aniversarios pagina inicial

--- webapp/views.py
+++ webapp/views.py
@@ -3,8 +3,10 @@ from django.core.urlresolvers import reverse
 from django.db.models import Sum, CharField
 from django.db.models.expressions import Value
 from django.db.models.functions import Concat
+from django.db.models.query_utils import Q
 from django.http.response import Http404
 from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from nucleo.models import Aniversario, Doacao
 from pagseguro.api import PagSeguroApi
 
@@ -12,26 +14,18 @@ from pagseguro.api import PagSeguroApi
 def index(request):
     aniversarios = Aniversario.objects.filter(
         finalizado__isnull=True
+    ).exclude(
+        Q(
+            ano__lt=timezone.now().year
+        ) | Q(
+            usuario__data_de_nascimento__month__lt=timezone.now().month,
+            ano=timezone.now().year
+        ) | Q(
+            usuario__data_de_nascimento__day__lt=timezone.now().day,
+            usuario__data_de_nascimento__month=timezone.now().month,
+            ano=timezone.now().year
+        )
     )
-    try:
-        aniversarios = aniversarios.annotate(
-            data_de_nascimento=Concat(
-                'usuario__data_de_nascimento',
-                Value(''),
-                output_field=CharField()
-            )
-        ).extra(
-            select={
-                'birthmonth': 'MONTH(data_de_nascimento)'
-            }
-        ).order_by(
-            'ano', 'birthmonth'
-        )
-        aniversarios.count()
-    except:
-        aniversarios = Aniversario.objects.filter(
-            finalizado__isnull=True
-        )
 
     aniversarios_realizados = Aniversario.objects.filter(
         finalizado__isnull=False,
@@ -40,6 +34,7 @@ def index(request):
     ).annotate(
         total_doacoes=Sum('doacoes__pagamento__valor')
     ).order_by('-total_doacoes')[:9]
+
     return render(request, 'webapp/index.html', {
         'aniversarios': aniversarios,
         'aniversarios_realizados': aniversarios_realizados
